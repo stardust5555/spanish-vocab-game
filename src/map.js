9ee07dea0300e18a7afe94cb529a0357bba20b86
shiva <shivana333@gmail.com>
2022-08-31 23:44:15 +0100

    Arranged the images for the quiz links onto the town map!

--- src/map.js
+++ src/map.js
@@ -1,4 +1,5 @@
 import townMap from "./townMap.png";
+import "./map.css"
 
 export default function Map(){
     const myStyle={
@@ -14,10 +15,11 @@ export default function Map(){
         <>
         <div style={myStyle}>
         <h1>Town Map</h1>
-        <img src={ require('./supermercado.png') } />
-        <img src={ require('./ropa.png') } />
-        <img src={ require('./barn.png') } />
-        <img src={ require('./escuela.png') } />
+        <img class="townMap" src={ require('./townMap.png') } alt="townMap"/>
+        <img class="supermarket" src={ require('./supermercado.png') } alt="supermarket"/>
+        <img class="clothesShop" src={ require('./ropa.png') } alt="clothes shop"/>
+        <img class="barn" src={ require('./barn.png') } alt="barn"/>
+        <img class="school" src={ require('./escuela.png') } alt="school"/>
         </div>
         </>
     )
