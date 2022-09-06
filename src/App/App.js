04d85da987fc068e6c2da1f5b564f89fec83b944
shiva <shivana333@gmail.com>
2022-09-06 15:49:05 +0100

    fixed unused import warnings in app.js to try and deploy to netlify

--- src/App/App.js
+++ src/App/App.js
@@ -1,5 +1,6 @@
 import "./App.css"
-import { BrowserRouter as Router, Routes, Link } from "react-router-dom"
+import { Link } from "react-router-dom"
+// import { BrowserRouter as Router, Routes, Link } from "react-router-dom"
 
 export default function App(){
     return(
